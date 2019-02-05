4a540eb6bada0d52ee35d53669d12aa99f7c6da5
s1tcomsfan <sitcomsfan@yandex.by>
2019-02-05 10:52:45 +0300

    add isPalindrom() function

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,12 +1,16 @@
-fizzBuzz();
-
 function fizzBuzz() {
     for (let i = 1; i <= 100; i++) {
         console.log(getFizzBuzzIfPossible(i));
     }
 
     function getFizzBuzzIfPossible(i) {
-        var x = ["FizzBuzz", i, i, "Fizz", i, "Buzz", "Fizz", i, i, "Fizz", "Buzz", i, "Fizz", i, i, i, i];
-        return x[i % 15];
-    };
-};
+        let results = ["FizzBuzz", i, i, "Fizz", i, "Buzz", "Fizz", i, i, "Fizz", "Buzz", i, "Fizz", i, i, i, i];
+        return results[i % 15];
+    }
+}
+
+function isPalindrom(str) {
+    let text = str.toLowerCase().replace(/\s+/g, "");
+    let reversedText = text.split("").reverse().join("");
+    return text == reversedText;
+}
