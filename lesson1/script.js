31023b1772e60cb10ae6c5db16b34b4c1afb9fc2
s1tcomsfan <sitcomsfan@yandex.by>
2019-02-09 10:10:57 +0300

    add isDeepEqual()

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,5 +1,3 @@
-fizzBuzz();
-
 function fizzBuzz() {
     for (let i = 1; i <= 100; i++) {
         console.log(getFizzBuzzIfPossible(i));
@@ -17,4 +15,34 @@ function isPalindrom(str) {
     let text = str.toLowerCase().replace(/\s+/g, "");
     let reversedText = text.split("").reverse().join("");
     return text == reversedText;
+}
+
+function isDeepEqual(a, b) {
+    if (a === b) {
+        return true;
+    }
+
+    if (a == null || typeof a != "object" ||
+        b == null || typeof b != "object") {
+        return false;
+    }
+
+    var propsContA = 0;
+    var propsContB = 0;
+    for (var propA in a) {
+        propsContA++;
+    };
+    for (var propB in b) {
+        propsContB++;
+    };
+    if (propsContA != propsContB) {
+        return false;
+    }
+
+    for (var key in a) {
+        if (!(key in b) || !isDeepEqual(a[key, b[key]])) {
+            return false;
+        }
+    }
+    return true;
 }
