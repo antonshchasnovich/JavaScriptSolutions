cb54b1440e0d295f60fa27d4a4db41b9fa5268ad
s1tcomsfan <sitcomsfan@yandex.by>
2019-02-10 10:14:27 +0300

    add drawCalendar()

--- lesson1/script.js
+++ lesson1/script.js
@@ -100,3 +100,39 @@ function quadraticEquation(a, b, c) {
         return [];
     }
 }
+
+drawCalendar(2019, 2, document.getElementById("calendar"));
+
+function drawCalendar(year, month, element) {
+    var d = new Date(year, month - 1);
+    var table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+    
+    for (var i = 0; i < getDay(d); i++) {
+        table += '<td></td>';
+    }
+    
+    while (d.getMonth() == month - 1) {
+        table += '<td>' + d.getDate() + '</td>';
+
+        if (d.getDay() == 0) {
+            table += '</tr><tr>';
+        }
+
+        d.setDate(d.getDate() + 1);
+    }
+    
+    if (getDay(d) != 0) {
+        for (var i = getDay(d); i < 7; i++) {
+            table += '<td></td>';
+        }
+    }
+    table += '</tr></table>';
+    element.innerHTML = table;
+    
+    function getDay(date) {
+      var day = date.getDay();
+      if (day == 0) day = 7;
+      return day - 1;
+    }
+
+}
